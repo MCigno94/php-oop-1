51ea85c8b7d83107667f4b911769f321c2d8b707
MCigno94 <mattiacigno27@gmail.com>
2022-05-25 15:44:03 +0200

    added comments

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
+/**
+ * Class Movie
+ * @author Mattia Cigno
+ * @copyright Movie di Mattia Cigno
+ */
 class Movie {
+    // dichiaro le mie variabili
     private $title;
     private $category;
     private $description;
 
+    // creo i metodi get per poter riutilizzare le variabili all'esterno
 	public function getTitle() {
 		return $this->title;
 	}
@@ -15,41 +22,45 @@ class Movie {
 		return $this->description;
 	}
 
+    // creo il mio costruttore passando le variabili da dichiarare alla creazione dll'oggetto 
     function __construct(String $title, String $category, String $description) {
         $this->title = $title;
         $this->category = $category;
         $this->description = $description;
     }
 
+    // creo il metodo play che si avvia quando dichiaro un titolo all'interno del mio oggetto
     function play(){
-        if($this->description !== null) {
+        if($this->title !== null) {
             echo "Il film è stato avviato";
         }
     }
 
 };
 
+// creo il primo oggetto Movie
 $transformers = new Movie("Transformers", "Azione/Fantascientifico", "Una voce narrante, quella di Optimus Prime, racconta dell'AllSpark, una fonte di energia a forma di cubo in grado di creare mondi e vita su di essi; due fazioni di esseri senzienti, una votata al bene e l'altra al male, si contendevano il suo potere sul proprio pianeta, che venne poi devastato e consumato. I robot partirono alla sua ricerca nell'universo, che dopo un lungo viaggio si perse, venendo poi localizzato sul pianeta Terra.");
 
-var_dump($transformers);
-//$title_a = $transformers->getTitle();
-//$category_a = $transformers->getCategory();
-//$description_a = $transformers->getDescription();
-//$transformers->play();
+    //var_dump($transformers);
+    //$title_a = $transformers->getTitle();
+    //$category_a = $transformers->getCategory();
+    //$description_a = $transformers->getDescription();
+    //$transformers->play();
 
+// creo il secondo oggetto Movie
 $fastEFurious = new Movie("Fast & Furious", "Azione", "La serie verte sulle vicende di due personaggi, Dominic Toretto, interpretato da Vin Diesel, e Brian O'Conner, interpretato da Paul Walker; tuttavia, a seguito della prematura scomparsa di Walker, il personaggio di O'Conner è stato fatto uscire di scena a partire dall'ottavo film. Negli anni la serie si è trasformata sempre più in un ensemble cast e ha visto aggiungersi, fra gli altri, icone del cinema d'azione come Dwayne Johnson, Jason Statham, Kurt Russell, Charlize Theron e John Cena.");
 
-var_dump($fastEFurious);
-//$title_b = $fastEFurious->getTitle();
-//$category_b = $fastEFurious->getCategory();
-//$description_b = $fastEFurious->getDescription();
-//$fastEFurious->play();
+    //var_dump($fastEFurious);
+    //$title_b = $fastEFurious->getTitle();
+    //$category_b = $fastEFurious->getCategory();
+    //$description_b = $fastEFurious->getDescription();
+    //$fastEFurious->play();
 
-
+// creo un nuovo array che utilizzerò per ciclare all'intenro dell'html
 $movies = [];
 array_push($movies, $transformers, $fastEFurious);
 
-var_dump($movies);
+    //var_dump($movies);
 
 ?>
 
